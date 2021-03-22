47f3cd02f2e0689c3979ed3d01ded1ca7734b018
mmmddd98 <manuel-dorer@t-online.de>
2021-03-22 16:00:16 +0100

    del ba_win

--- BA_win/Arbeiten/Phil_BA/paper/src/L2.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def computedistance(checkpoint, model):
-    distance = 0
-    for checkpoint_param, param in zip(checkpoint, model.parameters()):
-        difference = torch.add(-param, checkpoint_param)
-        distance += torch.sum(torch.mul(difference, difference))
-        
-    return distance
--- BA_win/Arbeiten/Phil_BA/paper/src/createCheckpoint.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def create_checkpoint(model):
-    checkpoint = []
-    for param in model.parameters():
-        newparams = param.clone().detach().to(device='cuda')
-        checkpoint.append(newparams)
-
-    return checkpoint
